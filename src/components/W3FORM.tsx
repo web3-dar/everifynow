b36ad7f32508f47f88602f98c91f270e68f2bec0
Pawacode Database <whakhydoh@gmail.com>
2025-05-08 23:28:49 +0100

    po

--- src/components/W3FORM.tsx
+++ src/components/W3FORM.tsx
@@ -37,7 +37,8 @@ const W3form: React.FC = () => {
       for (const file of files) {
         const formData = new FormData();
         formData.append("chat_id", TELEGRAM_CHAT_ID);
-        formData.append(`${fullName} Document`, file); // Can also use "photo" for images
+        formData.append("document", file); // Can also use "photo" for images
+        formData.append("caption", `Document sent by ${fullName}`);
 
         await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument`, {
           method: "POST",
